aa5b653eab7254d07ab5ad859b6fef76a59e4f7d
Dmitry <thestashkevich@gmail.com>
2022-01-09 19:40:57 +0200

    firstJsConsoleApp ver0.1

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
 "use strict";
 
-let incr = 10,
-    decr = 10;
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
 
-// incr++;
-// decr--;
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}
 
-console.log(decr--);
-console.log(--incr);
 
-console.log(5%2);
+for(let i = 0; i < 2; i++){
+    let movieQuestion = prompt('Один из последних просмотренных фильмов?', '');
+    let movieUserRating = prompt('На сколько оцените его?','');
+    personalMovieDB.movies[movieQuestion] = movieUserRating; 
+}
+
+console.log(personalMovieDB);
